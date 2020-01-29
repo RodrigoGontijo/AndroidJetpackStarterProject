316ef2bb5e901f4334a9a9d701e74ac5ce90a9d1
Unknown <rodrigo-gontijo@c-comp.grad.ufmg.br>
2020-01-29 17:36:54 -0300

    implement pallete

--- app/src/main/java/br/com/jetpackstarter/view/DogsDetailsFragment.kt
+++ app/src/main/java/br/com/jetpackstarter/view/DogsDetailsFragment.kt
@@ -1,6 +1,8 @@
 package br.com.jetpackstarter.view
 
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,13 +10,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.palette.graphics.Palette
 
 import br.com.jetpackstarter.R
 import br.com.jetpackstarter.databinding.FragmentDogsDetailsBinding
 import br.com.jetpackstarter.databinding.ItemDogBinding
+import br.com.jetpackstarter.model.dogsRepository.DogPalette
 import br.com.jetpackstarter.util.getProgressDrawable
 import br.com.jetpackstarter.util.loadImage
 import br.com.jetpackstarter.viewmodel.DetailDogViewModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import kotlinx.android.synthetic.main.fragment_dogs_details.*
 import kotlinx.android.synthetic.main.fragment_dogs_list.*
 import kotlinx.android.synthetic.main.item_dog.view.*
@@ -53,7 +60,32 @@ class DogsDetailsFragment : Fragment() {
         viewModel.dogLiveData.observe(this, Observer { dog ->
             dog?.let{
                 binding.dogDetail = dog
+                it.imageUrl?.let {
+                    setupBackgroundColor(it)
+                }
             }
         })
+    }
+
+    private fun setupBackgroundColor(url: String){
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>(){
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { pallete ->
+                            val intColor = pallete?.lightMutedSwatch?.rgb ?: 0
+                            val myPallete = DogPalette(intColor)
+                            binding.palette = myPallete
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+
+
+                }
+
+            })
     }
 }
